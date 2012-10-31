73bfeddb49cb18cd12f0fe361039626b74ee7d7f
John McCormack <jmccormack321@gmail.com>
2012-10-31 22:14:01 +0000

    project app setup with mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitnessJunction::Application.config.secret_token = 'eab70511aa13a78355b3983e55446d04e23b65404a18fbc3d13d16dfaed2e1c5a0040671d6768407881c23019612f4faf03b5aac7276dbd53044dd7e7e1cbf6d'
